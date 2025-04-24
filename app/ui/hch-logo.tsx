4d3efff55f0f4980aec6597e2c2ab0bdb7e71d71
iezll1106 <llizardo@byupathway.edu>
2025-04-24 15:58:08 +0800

    edit hch-logo.tsx

--- app/ui/hch-logo.tsx
+++ app/ui/hch-logo.tsx
@@ -3,14 +3,14 @@ import { lora } from '@/app/ui/fonts';
 
 export function HCHLogo() {
   return (
-    <div className="flex flex-col items-center justify-center w-full px-4 py-6 text-white md:flex-row md:space-x-8">
+    <div className="flex flex-col items-center justify-center w-full px-4 py-2 text-white md:flex-row md:space-x-8">
       {/* Left Logo */}
       <Image
         src="/logo_province.png"
         alt="Province Logo"
         width={80}
         height={80}
-        className="h-16 w-16 sm:h-20 sm:w-20 lg:h-24 lg:w-24 mb-4 md:mb-0"
+        className="h-14 w-14 sm:h-20 sm:w-20 lg:h-24 lg:w-24 mb-4 md:mb-0"
       />
 
       {/* Text Section */}
@@ -22,15 +22,6 @@ export function HCHLogo() {
           Republic of the Philippines
         </p>
       </div>
-
-      {/* Right Logo */}
-      <Image
-        src="/bp_logo.png"
-        alt="BP Logo"
-        width={80}
-        height={80}
-        className="h-16 w-16 sm:h-20 sm:w-20 lg:h-24 lg:w-24 mt-4 md:mt-0"
-      />
     </div>
   );
 }
@@ -55,7 +46,6 @@ export function SideNavLogo() {
         <p className={`${lora.className} text-lg sm:text-xl md:text-2xl lg:text-3xl`}>
           Republic of the Philippines
         </p>
-      </div>
 
       {/* Right Logo */}
       <Image
@@ -65,6 +55,7 @@ export function SideNavLogo() {
         height={80}
         className="h-16 w-16 sm:h-20 sm:w-20 lg:h-24 lg:w-24 mt-4 md:mt-0"
       />
+      </div>
     </div>
   );
 }
