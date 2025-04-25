11e68485f865aec80e36f51614b6f400f88d0ceb
iezll1106 <llizardo@byupathway.edu>
2025-04-25 15:28:02 +0800

    final main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,5 @@ export default function Page() {
           </div>
         </div>
       </main>
-    {/* <footer className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"> Wednesday</footer> */}
   </>
 }
